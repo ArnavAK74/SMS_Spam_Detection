bdbdd6986e27ea94c39cfed5a6e56bb25efabeb0
Arnav74 <arnavak55@gmail.com>
2025-01-01 18:58:47 -0500

    Modify main.py

--- main.py
+++ main.py
@@ -48,7 +48,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "API is running. Use /predict to test predictions."}
+    return {"message": "API is running"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 # Define input schema
 class SpamInput(BaseModel):
